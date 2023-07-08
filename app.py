4bc7a571b2a35a14c75df3d29962454dc7542228
Ed Denham <ed.denham@gmail.com>
2023-07-08 17:39:14 +1200

    feat: add Factos of Fraud study page to streamlit app

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 
+from app_pages.page_factors_of_fraud import page_factors_of_fraud_body
 
 app = MultiPage(app_name="Auckland Capital Bank's Frauditor")
-
+app.add_page("Factors of Fraud TBC", page_factors_of_fraud_body)
 
 app.run()
---
+++ app_pages/page_factors_of_fraud.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page_factors_of_fraud_body():
+    st.info("Factors of Fraud tbc")
