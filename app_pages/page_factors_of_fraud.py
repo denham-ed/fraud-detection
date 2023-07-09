f941d50b3267209d928f584bf1f1447dd93632b0
Ed Denham <ed.denham@gmail.com>
2023-07-09 14:05:36 +1200

    feat: add introduction to factors of fraud study dashboard page

--- app_pages/page_factors_of_fraud.py
+++ app_pages/page_factors_of_fraud.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 def page_factors_of_fraud_body():
-    st.info("Factors of Fraud tbc")
+    st.header("Factors of Fraud")
+
+    st.info(
+        "The senior management team at Auckland Capital Bank (ACB) "
+        "want to better undeststand the patterns from their transaction data "
+        "in order to identify the most relevant variables correlated "
+        "to a churned customer."
+    )
+
+    
